08881b3309fa1dd2b559a8a775057581c1259b72
Alex Mas <lex77734@gmail.com>
2022-12-17 12:25:25 +0200

    added check existence of dir for canvas snap on server

--- server/src/services/image.service.ts
+++ server/src/services/image.service.ts
@@ -3,18 +3,23 @@ import fs from 'fs'
 import path from 'path'
 
 const __dirname = path.resolve()
+const dir = path.resolve(__dirname, 'src', 'files')
+const filePath = (id: string) => path.resolve(dir, `${id}.jpg`)
 
 export const write = (img: string, id: string) => {
   if (img.startsWith(imagePrefix)) {
     const data = img.replace(imagePrefix, '')
-    fs.writeFileSync(path.resolve(__dirname, 'src', 'files', `${id}.jpg`), data, 'base64')
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    fs.writeFileSync(filePath(id), data, 'base64')
     return 'Downloaded successfully'
   }
   throw new Error(`Received value is not started for required prefix [${imagePrefix}]`)
 }
 
 export const read = (id: string) => {
-  const file = fs.readFileSync(path.resolve(__dirname, 'src', 'files', `${id}.jpg`))
+  const file = fs.readFileSync(filePath(id))
   const data = imagePrefix + file.toString('base64')
   return data
 }
